4a2ae7ae98dcc1f4bb558e8a62e8b525dcbd7ab9
Saradlr1021 <125328737+Saradlr1021@users.noreply.github.com>
2023-10-16 13:11:28 -0700

    added the GitHub Repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return  <div className="App">Weather App</div>;
+  return  <div className="App">
+  <h1>Weather App</h1>
+    <footer>
+      This project was coded by Sara P. Reyes and is{" "}
+    <a href="https://github.com/Saradlr1021/react-weather-app" target="_blank">
+      open-sourced on GitHub
+    </a>
+    </footer>
+    </div>;
 }
 
