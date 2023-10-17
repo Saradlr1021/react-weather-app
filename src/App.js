aadd3c3d2922e4eb46ffda765bb8c8cc27477310
Saradlr1021 <125328737+Saradlr1021@users.noreply.github.com>
2023-10-17 09:31:59 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-
 import './App.css';
 
 export default function App() {
-  return  <div className="App">
+  return ( <div className="App">
+    <div className="container">
   <h1>Weather App</h1>
     <footer>
       This project was coded by Sara P. Reyes and is{" "}
@@ -12,6 +12,8 @@ export default function App() {
       open-sourced on GitHub
     </a>
     </footer>
-    </div>;
+    </div>
+    </div>
+);
 }
 
