9ba3f7a73381d243b34843a3df1d5b5931285bea
Saradlr1021 <125328737+Saradlr1021@users.noreply.github.com>
2023-10-17 14:45:52 -0700

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import Weather from "./Weather"
 import './App.css';
 
 export default function App() {
   return ( <div className="App">
     <div className="container">
-  <h1>Weather App</h1>
+  <Weather />
     <footer>
       This project was coded by Sara P. Reyes and is{" "}
     <a href="https://github.com/Saradlr1021/react-weather-app" target="_blank">
